00ebe2e471a7d76ae5be99b969345cb77a190c06
Akhil K J <akhilkj@valorem.com>
2018-07-03 17:37:51 +0530

    inital calculator files code drop

---
+++ CalculatorTest/Class1.cs
@@ -0,0 +1,41 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+using UnityEngine;
+
+public class Calculate
+{
+    //public static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+    public static NLog.Logger logger = LogManager.GetLogger("Calculate");
+
+    public delegate void LogReceived(string foo);
+
+    public event LogReceived Logged;
+
+    public int Div(int a, int b)
+    {
+        var config = new LoggingConfiguration();
+        var fileTarget = new FileTarget("targetFile")
+        {
+            FileName = "C:/Users/Akhilkj/Documents/CalculatorTest/Logs/Test.log",
+            Layout = "${longdate} ${level} ${message}  ${exception}"
+        };
+        config.AddTarget(fileTarget);
+        config.AddRuleForOneLevel(LogLevel.Error, fileTarget);
+        LogManager.Configuration = config;
+        try
+        {
+            int x = a / b;
+            Logged("dllLog " + x.ToString());
+            //Logs to Unity Console
+            Debug.Log("logtoUnityTest");
+            return x;
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex);
+        }
+        return 0;
+    }
+}
