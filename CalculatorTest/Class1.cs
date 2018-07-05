69548e7a54b34fb1b3702f93742737c69f62a5ba
Akhil K J <akhilkj@valorem.com>
2018-07-05 14:57:44 +0530

    target changes

--- CalculatorTest/Class1.cs
+++ CalculatorTest/Class1.cs
@@ -1,6 +1,4 @@
 ﻿using NLog;
-using NLog.Config;
-using NLog.Targets;
 using System;
 using UnityEngine;
 
@@ -9,27 +7,15 @@ public class Calculate
     //public static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
     public static NLog.Logger logger = LogManager.GetLogger("Calculate");
 
-    public delegate void LogReceived(string foo);
-
-    public event LogReceived Logged;
-
     public int Div(int a, int b)
     {
-        var config = new LoggingConfiguration();
-        var fileTarget = new FileTarget("targetFile")
-        {
-            FileName = "C:/Users/Akhilkj/Documents/CalculatorTest/Logs/Test.log",
-            Layout = "${longdate} ${level} ${message}  ${exception}"
-        };
-        config.AddTarget(fileTarget);
-        config.AddRuleForOneLevel(LogLevel.Error, fileTarget);
-        LogManager.Configuration = config;
+        // logs to file specified in Unity
+        logger.Error("dllError");
         try
         {
             int x = a / b;
-            Logged("dllLog " + x.ToString());
-            //Logs to Unity Console
-            Debug.Log("logtoUnityTest");
+            logger.Debug("dlllog: " + x);
+            Debug.Log("logtoUnityConsoleTest");
             return x;
         }
         catch (Exception ex)
